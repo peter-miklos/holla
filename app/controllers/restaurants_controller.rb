fefba5d7be00c1d92b091bf67e524d8aaea8ed92
James Stonehill <jamesstonehill@gmail.com>
2016-10-03 15:49:57 +0100

    addign controler logic

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,16 @@
 class RestaurantsController < ApplicationController
   def index
+    @restaurants = Restaurant.all
     # raise 'Hello from the index action'
+  end
+
+  def new
+    @restaurant = Restaurant.new
+    # this is going to need to be changed when user functionality is implimented
+  end
+
+  def create
+    @restaurant = Restaurant.create(restaurant_params)
+    redirect_to '/restaurants'
   end
 end
