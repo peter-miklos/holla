b86281f6122ef4bcef7d6aa944b3ab5bb08a21b5
Peter Miklos <petermiklos@Peters-MacBook-Pro.local>
2016-10-04 14:22:38 +0100

    review feature test fixed, review#index added

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,7 +8,16 @@ class ReviewsController < ApplicationController
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
     @restaurant.reviews.create(review_params)
-    redirect_to '/restaurants'
+    redirect_to "/restaurants/#{params[:restaurant_id]}/reviews"
+
+  end
+
+  def show
+    @review = Review.find(params[:id])
+  end
+
+  def index
+    @reviews = Review.where(restaurant_id: params[:restaurant_id])
   end
 
   private
--- spec/features/review_feature_spec.rb
+++ spec/features/review_feature_spec.rb
@@ -2,17 +2,16 @@ require "rails_helper"
 
 feature "reviews" do
 
-  before { Restaurant.create name: 'KFC'}
-  # let!(:kfc){ Restaurant.create(name: "KFC", address: "London", description: "chicken") }
+  let!(:kfc){ Restaurant.create(name: "KFC", address: "London", description: "chicken") }
 
   scenario "user can add a review to a restaurant" do
-    visit "/restaurants"
-    click_link "Review KFC"
+    visit "/restaurants/#{kfc.id}"
+    click_link "Add review"
     select("4", from: "Rating")
     fill_in("Comment", with: "Tasty chicken")
     click_button "Create Review"
 
-    # expect(page).to have_content("Tasty chicken")
-    expect(current_path).to eq "/restaurants"
+    expect(page).to have_content("Tasty chicken")
+    expect(current_path).to eq "/restaurants/#{kfc.id}/reviews"
   end
 end
