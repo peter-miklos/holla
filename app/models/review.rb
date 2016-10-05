056ad99b1523653644a7314731a12cd386929495
James Stonehill <jamesstonehill@gmail.com>
2016-10-05 12:14:41 +0100

    user can not review the same restaurant twice

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,8 @@ class Review < ApplicationRecord
 
   belongs_to :user
   has_one :restaurant
+
+  validates :user_id, uniqueness: { scope: :restaurant_id, message: "You have already reviewed this restaurant" }
+  # validates_uniqueness_of :user_id, :scope => :restaurant_id, message: "You have already reviewed this restaurant"
+
 end
