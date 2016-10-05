c47854f6b8e9e5f1bb6434164d8016a9105fa91c
lsewilson <laurawilson@hotmail.co.uk>
2016-10-05 11:03:10 +0100

    adds user email on review

--- spec/features/review_feature_spec.rb
+++ spec/features/review_feature_spec.rb
@@ -1,13 +1,13 @@
 require "rails_helper"
 
 feature "reviews" do
-  let!(:user){ User.create(email: "Laura@troll.com", password: "123456") }
+  let!(:user){ User.create(email: "laura@troll.com", password: "123456") }
   let!(:kfc){ Restaurant.create(name: "KFC", address: "London", description: "chicken", user_id: user.id) }
 
   before do
     visit('/')
     click_link('Sign in')
-    fill_in('Email', with: 'Laura@troll.com')
+    fill_in('Email', with: 'laura@troll.com')
     fill_in('Password', with: '123456')
     click_button("Log in")
   end
@@ -21,6 +21,14 @@ feature "reviews" do
 
     expect(current_path).to eq "/restaurants/#{kfc.id}/reviews"
     expect(page).to have_content("Tasty chicken")
+  end
 
+  scenario "users can see who review belongs to" do
+    visit "/restaurants/#{kfc.id}"
+    click_link "Add review"
+    select("4", from: "Rating")
+    fill_in("Comment", with: "Tasty chicken")
+    click_button "Create Review"
+    expect(page).to have_content("laura@troll.com")
   end
 end
