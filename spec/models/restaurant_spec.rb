c8dfd912c775d08b420a00574941b2bfba8ebf7b
Peter Miklos <pt.miklos@gmail.com>
2016-10-07 13:22:32 +0100

    restaurant unit tests added

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -3,6 +3,10 @@ require "rails_helper"
 describe Restaurant, type: :model do
 
   let!(:user){ User.create(email: "Laura@troll.com", password: "123456") }
+  let!(:bk){ Restaurant.create(name: "Burger King", address: "London", description: "burger", user_id: user.id) }
+  let!(:kfc_l){ Restaurant.create(name: "KFC London", address: "London", description: "chicken", user_id: user.id) }
+  let!(:kfc_b){ Restaurant.create(name: "KFC Bristol", address: "Bristol", description: "chicken", user_id: user.id) }
+
 
   it "does not save the restaurant with a name of less than three chars " do
     restaurant = Restaurant.new(name: "Mc")
@@ -14,5 +18,15 @@ describe Restaurant, type: :model do
     Restaurant.create(name: "Dirty Bones", user_id: user.id)
     restaurant = Restaurant.new(name: "Dirty Bones", user_id: 1)
     expect(restaurant).to have(1).error_on(:name)
+  end
+
+  it "returns the restaurants containing the keyword" do
+    restults = Restaurant.search("KFC")
+    expect(restults).to eq [kfc_l, kfc_b]
+  end
+
+  it "returns all the restaurants if no keyword is used" do
+    restults = Restaurant.search("")
+    expect(restults).to eq [bk, kfc_l, kfc_b]
   end
 end
